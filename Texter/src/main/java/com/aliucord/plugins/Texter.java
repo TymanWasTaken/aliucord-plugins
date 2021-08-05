31d0b1d180c784e5418f4b1970e60c676395c2bc
TymanWasTaken <tyman@tyman.tech>
2021-08-05 10:59:38 -0600

    bump version

--- Texter/src/main/java/com/aliucord/plugins/Texter.java
+++ Texter/src/main/java/com/aliucord/plugins/Texter.java
@@ -29,7 +29,7 @@ public class Texter extends Plugin {
         var manifest = new Manifest();
         manifest.authors = new Manifest.Author[]{new Manifest.Author("Tyman", 487443883127472129L)};
         manifest.description = "A plugin to modify text formatting inspired by plugins like BetterFormattingRedux (for BetterDiscord) and texter (for Powercord)";
-        manifest.version = "1.0.0";
+        manifest.version = "1.0.1";
         manifest.updateUrl = "https://raw.githubusercontent.com/TymanWasTaken/aliucord-plugins/builds/updater.json";
         return manifest;
     }
