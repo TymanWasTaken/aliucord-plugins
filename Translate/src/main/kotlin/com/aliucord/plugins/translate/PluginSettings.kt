9badfa5dbe6492ad9e55e3dfec9f3c790e6b3bb4
TymanWasTaken <tyman@tyman.tech>
2021-08-29 11:51:07 -0600

    Translate v1.2.1

--- Translate/src/main/kotlin/com/aliucord/plugins/translate/PluginSettings.kt
+++ Translate/src/main/kotlin/com/aliucord/plugins/translate/PluginSettings.kt
@@ -1,26 +1,26 @@
 package com.aliucord.plugins.translate
 
 import android.text.Editable
+import android.text.util.Linkify
 import android.view.View
-
+import android.widget.TextView
 import com.aliucord.views.TextInput
-
 import com.aliucord.Utils
-
 import com.aliucord.api.SettingsAPI
 import com.aliucord.fragments.SettingsPage
 import com.aliucord.views.Button
+import com.discord.utilities.color.ColorCompat
 import com.discord.utilities.view.text.TextWatcher
+import com.lytefast.flexinput.R
 
 
 class PluginSettings(private val settings: SettingsAPI) : SettingsPage() {
-    val regex = Regex("^[a-z]{2}\$")
     override fun onViewBound(view: View?) {
         super.onViewBound(view)
         setActionBarTitle("Translate")
         val ctx = requireContext()
         val input = TextInput(ctx)
-        input.hint = "Language code to translate messages to by default (default en)"
+        input.hint = "Language to translate messages to by default"
         val editText = input.editText!!
         val button = Button(ctx)
         button.text = "Save"
@@ -35,7 +35,7 @@ class PluginSettings(private val settings: SettingsAPI) : SettingsPage() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable) {
-                if (!regex.matches(s.toString())) {
+                if (!languageCodes.containsValue(s.toString())) {
                     button.alpha = 0.5f
                     button.isClickable = false
                 } else {
@@ -44,7 +44,13 @@ class PluginSettings(private val settings: SettingsAPI) : SettingsPage() {
                 }
             }
         })
+        val helpText = TextView(ctx)
+        helpText.linksClickable = true
+        helpText.text = "Supported language codes:\n\n${languageCodes.map { "${it.key} -> ${it.value}" }.joinToString("\n")}"
+        Linkify.addLinks(helpText, Linkify.WEB_URLS)
+        helpText.setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorOnPrimary))
         addView(input)
         addView(button)
+        addView(helpText)
     }
 }
