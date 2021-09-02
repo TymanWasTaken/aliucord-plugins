04e089728757e499ebf3b39430a842387381d99d
TymanWasTaken <tyman@tyman.tech>
2021-09-01 20:03:11 -0600

    Finished I think

--- CallTime/src/main/kotlin/com/aliucord/plugins/CallTime.kt
+++ CallTime/src/main/kotlin/com/aliucord/plugins/CallTime.kt
@@ -1,7 +1,7 @@
 package com.aliucord.plugins
 
 import android.content.Context
-import com.aliucord.Logger
+import com.aliucord.Utils
 import com.aliucord.entities.Plugin.Manifest.Author
 import com.aliucord.patcher.PinePatchFn
 import com.aliucord.api.PatcherAPI
@@ -23,6 +23,19 @@ class CallTime : Plugin() {
     private var cachedVoiceState: OutgoingPayload.VoiceStateUpdate? = null
     private val timer = Timer()
     private var timerTask: TimerTask? = null
+    private val clock = ClockFactory.get()
+    private val elapsedTime: String
+        get() = run {
+            val vcConnectedTime = vcConnectedTime ?: return "00:00:00"
+            val duration = clock.currentTimeMillis() - vcConnectedTime
+            val hoursNumber = TimeUnit.MILLISECONDS.toHours(duration)
+            val minutesNumber = TimeUnit.MILLISECONDS.toMinutes(duration) - hoursNumber * 60
+            val secondsNumber = TimeUnit.MILLISECONDS.toSeconds(duration) - minutesNumber * 60
+            val hours = String.format("%02d", hoursNumber)
+            val minutes = String.format("%02d", minutesNumber)
+            val seconds = String.format("%02d", secondsNumber)
+            return "$hours:$minutes:$seconds"
+        }
 
     override fun getManifest() = Manifest().apply {
         authors = arrayOf(Author("Tyman", 487443883127472129L))
@@ -36,20 +49,22 @@ class CallTime : Plugin() {
                 """.trimIndent()
     }
 
-    override fun start(context: Context) {
-        patcher.patchConnectedText()
+    override fun start(ctx: Context) {
+        patcher.patchConnectedText(ctx)
     }
 
-    private fun PatcherAPI.patchConnectedText() {
+    private fun PatcherAPI.patchConnectedText(ctx: Context) {
         val currentVoiceStateField = StoreRtcConnection::class.java.getDeclaredField("currentVoiceState").apply { isAccessible = true }
-        val onVoiceStateUpdateMethod = StoreRtcConnection::class.java.getDeclaredMethod("onVoiceStateUpdated").apply { isAccessible = true }
         val setupIndicatorStatusMethod = WidgetGlobalStatusIndicator::class.java.getDeclaredMethod("setupIndicatorStatus", WidgetGlobalStatusIndicatorViewModel.ViewState.CallOngoing::class.java).apply { isAccessible = true }
-        val clock = ClockFactory.get()
+        val getConnectedTextMethod = VoiceViewUtils::class.java.getDeclaredMethod("getConnectedText", Context::class.java, RtcConnection.State::class.java, StreamContext::class.java, Boolean::class.javaPrimitiveType)
 
         patch(setupIndicatorStatusMethod, PinePatchFn {
             val widget = it.thisObject as WidgetGlobalStatusIndicator
-            timerTask = timer.schedule(0, 1000) {
-                setupIndicatorStatusMethod.invoke(widget, it.args[0])
+            timerTask?.cancel()
+            timerTask = timer.schedule(1000) {
+                widget.activity?.runOnUiThread {
+                    setupIndicatorStatusMethod.invoke(widget, it.args[0])
+                }
             }
         })
 
@@ -57,22 +72,18 @@ class CallTime : Plugin() {
             val store = it.thisObject as StoreRtcConnection
             val currentVoiceState = currentVoiceStateField[store] as OutgoingPayload.VoiceStateUpdate?
                 ?: return@PinePatchFn
-            vcConnectedTime = if (currentVoiceState.channelId != null && cachedVoiceState?.channelId == null) {
+            vcConnectedTime = if (currentVoiceState.channelId != null && cachedVoiceState?.channelId != currentVoiceState.channelId) {
                 clock.currentTimeMillis()
             } else {
                 timerTask?.cancel()
                 timerTask = null
+                if (vcConnectedTime != null) Utils.showToast(ctx, "Call ended. Elapsed time: $elapsedTime", true)
                 null
             }
             cachedVoiceState = currentVoiceState
         })
-        patch(VoiceViewUtils::class.java.getDeclaredMethod("getConnectedText", Context::class.java, RtcConnection.State::class.java, StreamContext::class.java, Boolean::class.javaPrimitiveType), PinePatchFn {
-            val vcConnectedTime = vcConnectedTime ?: return@PinePatchFn
-            val duration = clock.currentTimeMillis() - vcConnectedTime
-            val hours = String.format("%02d", TimeUnit.MILLISECONDS.toHours(duration))
-            val minutes = String.format("%02d", TimeUnit.MILLISECONDS.toMinutes(duration))
-            val seconds = String.format("%02d", TimeUnit.MILLISECONDS.toSeconds(duration))
-            it.result = "${it.result} ($hours:$minutes:$seconds)"
+        patch(getConnectedTextMethod, PinePatchFn {
+            it.result = "${it.result} ($elapsedTime)"
         })
 
     }
