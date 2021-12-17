5bab14c3f13d514f6c30934ac230f86d10fd1698
TymanWasTaken <tyman@tyman.tech>
2021-12-17 16:35:39 -0700

    fix???

--- .github/getchanged.py
+++ .github/getchanged.py
@@ -1,10 +1,10 @@
-import subprocess, re, sys
+import subprocess, re, sys, os
 
 global_change = re.compile(r"^(?:(?:build|settings)\.gradle(?:\.kts)?|gradle\.properties|\.github\/(?:workflows\/.+|getchanged\.py)|\.gradle\/.+)$")
 plugin_change = re.compile(r"^(.+)\/(?:src\/main\/.+|(?:build|settings)\.gradle(?:\.kts)?)$")
 
 sha = sys.argv[1]
-out = subprocess.run(["git", "diff", "--name-only", sha, f"{sha}~1"], stdout=subprocess.PIPE).stdout.strip(b'\n').decode("utf-8")
+out = subprocess.run(["git", "diff", "--name-only", sha, f"{sha}~1"], stdout=subprocess.PIPE, cwd=f"{os.environ['GITHUB_WORKSPACE']}/src").stdout.strip(b'\n').decode("utf-8")
 
 matched = ""
 found = []
